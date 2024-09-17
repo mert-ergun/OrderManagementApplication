01416a5cb4d4b21f58cab92af261e77f6b610f29
Mert ERGÜN <mertergun17@hotmail.com>
2024-09-17 14:23:36 +0300

    refactor: Enforce email and password validation on login

--- src/business/UserController.java
+++ src/business/UserController.java
@@ -1,5 +1,6 @@
 package business;
 
+import core.Utils;
 import dao.UserDAO;
 import entity.User;
 
@@ -7,6 +8,10 @@ public class UserController {
     private final UserDAO userDAO = new UserDAO();
 
     public User login(String email, String password) {
-        return userDAO.getUser(email, password);
+        if (Utils.isEmailValid(email) && Utils.isPasswordValid(password.toCharArray())) {
+            return userDAO.getUser(email, password);
+        } else {
+            return null;
+        }
     }
 }
