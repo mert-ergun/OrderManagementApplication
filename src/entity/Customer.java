e57dd2cf00d986112287fa926d99b743c741d62b
Mert ERGÜN <mertergun17@hotmail.com>
2024-09-17 14:55:15 +0300

    feat: Introduce Customer entity

---
+++ src/entity/Customer.java
@@ -0,0 +1,86 @@
+package entity;
+
+public class Customer {
+    private int id;
+    private String name;
+    private CustomerType type;
+    private String phone;
+    private String email;
+    private String address;
+
+    public enum CustomerType {
+        INDIVIDUAL, CORPORATE
+    }
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name, CustomerType type, String phone, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CustomerType getType() {
+        return type;
+    }
+
+    public void setType(CustomerType type) {
+        this.type = type;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type=" + type +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
