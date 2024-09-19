9cefdad1f06017b57adc52556ff683934d071795
Mert ERGÜN <mertergun17@hotmail.com>
2024-09-19 11:17:16 +0300

    feat: Introduce Cart entity

---
+++ src/entity/Cart.java
@@ -0,0 +1,40 @@
+package entity;
+
+public class Cart {
+    private int id;
+    private int productId;
+    private Product product;
+
+    public Cart() {
+    }
+
+    public Cart(int id, int productId, Product product) {
+        this.id = id;
+        this.productId = productId;
+        this.product = product;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
